6412722ff689b7e9325f07b11f55d539e437d485
Murilo <43999445+murilobellatini@users.noreply.github.com>
2022-06-06 21:34:00 +0200

    Fix bat

--- scripts/test_train.bat
+++ scripts/test_train.bat
@@ -1,4 +1,4 @@
 set MODEL_FLAGS=--image_size 64 --num_channels 128 --num_res_blocks 3  &
-set DIFFUSION_FLAGS=--diffusion_steps 20 --noise_schedule linear  &
+set DIFFUSION_FLAGS=--diffusion_steps 100 --noise_schedule linear  &
 set TRAIN_FLAGS=--lr 1e-4 --batch_size 10  &
 python scripts/image_train.py --data_dir ./data/raw/test/resized %MODEL_FLAGS% %DIFFUSION_FLAGS% %TRAIN_FLAGS% 
