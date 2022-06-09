540e26be799794da517c8fa1ad95fb91ce1f05e8
Murilo <43999445+murilobellatini@users.noreply.github.com>
2022-06-09 13:29:36 +0200

    Format pep8

--- guided_diffusion/src/helpers.py
+++ guided_diffusion/src/helpers.py
@@ -11,7 +11,7 @@ import mimetypes
 mimetypes.init()
 
 
-def resize_images(input_dir:pl.Path, size:tuple, output_dir:pl.Path=None, new_suffix='.jpg') -> bool:
+def resize_images(input_dir: pl.Path, size: tuple, output_dir: pl.Path = None, new_suffix='.jpg') -> bool:
     """Resizes all images within folder.
 
     Args:
@@ -27,9 +27,9 @@ def resize_images(input_dir:pl.Path, size:tuple, output_dir:pl.Path=None, new_su
     """
     if not output_dir:
         output_dir = input_dir / 'resized'
-        
+
     safe_makedirs(output_dir)
-        
+
     for f in tqdm(os.listdir(input_dir)):
 
         ipath = input_dir / f
@@ -47,10 +47,11 @@ def resize_images(input_dir:pl.Path, size:tuple, output_dir:pl.Path=None, new_su
         iimg = Image.open(ipath)
         oimg = iimg.resize(size)
         oimg.save(opath)
-        
+
     return True
 
-def safe_makedirs(dirs:pl.Path) -> bool:
+
+def safe_makedirs(dirs: pl.Path) -> bool:
     """Creates nested directories safely.
 
     Args:
@@ -64,35 +65,53 @@ def safe_makedirs(dirs:pl.Path) -> bool:
     return True
 
 
-def array2tuple(arr:np.array) -> tuple:
+def array2tuple(arr: np.array) -> tuple:
     """
     Converts array to tuple
     """
     return tuple(map(lambda x: tuple(x)[0], arr))
 
-def get_arange_matrix(shape:tuple) -> np.array:
+
+def get_arange_matrix(shape: tuple) -> np.array:
     """
     Generates arange in matrix format (given shape)
     """
     return np.arange(np.prod(shape)).reshape(shape)
 
-def get_ij(ix:tuple, subplots_size=(2,3)) -> tuple:
+
+def get_ij(ix: tuple, subplots_size=(2, 3)) -> tuple:
     """
     Gets indexes i,j from tuple ix on subplot grid
     """
     arange_matrix = get_arange_matrix(subplots_size)
     return array2tuple(np.where(ix == arange_matrix))
 
-def turn_off_axes(axes:np.array):
+
+def turn_off_axes(axes: np.array) -> None:
     """
     Turn offs all subplot axes
     """
     [axi.set_axis_off() for axi in axes.ravel()]
-    
-def render_samples(spath:pl.Path, samples2render:int=10, subplots_size=(5,2), title=None) -> figure.Figure:
+
+
+def get_figsize(subplots_size: tuple, scaling_factor: int = 2) -> tuple:
+    """Gets figsize from subplots grid size
+
+    Args:
+        subplots_size (tuple): Subplots grid size
+        scaling_factor (int, optional): Scaling factor of figsize w.r.t. subplots_size. Defaults to 2.
+
+    Returns:
+        tuple: Figsize dimension
+    """
+    x, y = tuple(map(lambda x: scaling_factor*x, subplots_size))
+    return y, x
+
+
+def render_samples(spath: pl.Path, samples2render: int = 10, subplots_size=(5, 2), title=None) -> figure.Figure:
     """
     Displays in IPython notebook the images produced by the model.
-    
+
     It renders the numpy.array output by image_sample.py
 
     Args:
@@ -104,10 +123,10 @@ def render_samples(spath:pl.Path, samples2render:int=10, subplots_size=(5,2), ti
     Returns:
         figure.Figure: Plot with rendered samples
     """
-    assert samples2render <= np.prod(subplots_size), f'`samples2render` ({samples2render}) does not fit `subplots_size` ({subplots_size}): {samples2render} > {np.prod(subplots_size)}'
-    
+    assert samples2render <= np.prod(
+        subplots_size), f'`samples2render` ({samples2render}) does not fit `subplots_size` ({subplots_size}): {samples2render} > {np.prod(subplots_size)}'
+
     image_arrays = np.load(spath)["arr_0"]
-    
     samples2render = min(image_arrays.shape[0], samples2render)
 
     images = []
@@ -119,19 +138,19 @@ def render_samples(spath:pl.Path, samples2render:int=10, subplots_size=(5,2), ti
     f, axarr = plt.subplots(
         nrows=subplots_size[0],
         ncols=subplots_size[1],
-        figsize=tuple(map(lambda x: 2*x, (5,2))))
-    
-    for ix, img in enumerate(images):
-        if ix > len(images):
-            break
-        i,j = get_ij(ix, subplots_size)
-        axarr[i,j].imshow(img)
-        axarr[i,j].set_title("Sample = %s" % ix)
+        figsize=get_figsize(subplots_size),
+        squeeze=False
+    )
 
+    for ix, img in enumerate(images):
+        if ix > np.prod(subplots_size):
+            break
+        i, j = get_ij(ix, subplots_size)
+        axarr[i, j].imshow(img)
+        axarr[i, j].set_title("Sample = %s" % ix)
         turn_off_axes(axarr)
-        
+
     if title:
-        plt.suptitle(title)
-        
+        plt.suptitle(title, y=1.20)
+
     return f
-    
