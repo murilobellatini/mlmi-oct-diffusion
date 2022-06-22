a27ce25eb1f5d36e14c90c9fee2d46b6b655504a
Murilo <43999445+murilobellatini@users.noreply.github.com>
2022-06-22 12:21:39 +0200

    Add experiment exploration

--- guided_diffusion/src/paths.py
+++ guided_diffusion/src/paths.py
@@ -8,4 +8,5 @@ import pathlib as pl
 root_path = pl.Path(__file__).parents[2]
 DATA_PATH = root_path / 'data'
 RAW_DATA_PATH = root_path / 'data/raw'
+LOGS_DATA_PATH = root_path / 'data/logs'
 PROCESSED_DATA_PATH = root_path / 'data/raw'
