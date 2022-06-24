dbc56b71d451fbe73fe723a76f988f129ff47e08
ge32woz <ge32woz@tum.de>
2022-06-24 17:22:36 +0200

    changed to yaml based config

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -4,6 +4,8 @@ Train a diffusion model on images.
 
 import json
 import argparse
+import yaml
+import click
 
 from guided_diffusion import dist_util, logger
 from guided_diffusion.image_datasets import load_data
@@ -16,28 +18,32 @@ from guided_diffusion.script_util import (
 )
 from guided_diffusion.train_util import TrainLoop
 
-
-def main():
-    args = create_argparser().parse_args()
+@click.command()
+@click.argument("params", type=click.File("r"))
+def main(params_file):
+    params = get_default_params()
+    params.update(yaml.safe_load(params_file))
 
     dist_util.setup_dist()
     logger.configure()
 
     logger.log("creating model and diffusion...")
-    logger.log(f"loaded arguments are: {json.dumps(vars(args), indent=4)}")
+    logger.log(f"loaded arguments are: {json.dumps(vars(params), indent=4)}")
+    
     model, diffusion = create_model_and_diffusion(
-        **args_to_dict(args, model_and_diffusion_defaults().keys())
+        **params
     )
     model.to(dist_util.dev())
     schedule_sampler = create_named_schedule_sampler(
-        args.schedule_sampler, diffusion)
+        params.schedule_sampler, diffusion
+    )
 
     logger.log("creating data loader...")
     data = load_data(
-        data_dir=args.data_dir,
-        batch_size=args.batch_size,
-        image_size=args.image_size,
-        class_cond=args.class_cond,
+        data_dir=params["data_dir"],
+        batch_size=params["batch_size"],
+        image_size=params["image_size"],
+        class_cond=params["class_cond"],
     )
 
     logger.log("training...")
@@ -45,23 +51,23 @@ def main():
         model=model,
         diffusion=diffusion,
         data=data,
-        batch_size=args.batch_size,
-        microbatch=args.microbatch,
-        lr=args.lr,
-        ema_rate=args.ema_rate,
-        log_interval=args.log_interval,
-        save_interval=args.save_interval,
-        resume_checkpoint=args.resume_checkpoint,
-        use_fp16=args.use_fp16,
-        fp16_scale_growth=args.fp16_scale_growth,
+        batch_size=params["batch_size"],
+        microbatch=params["microbatch"],
+        lr=params["lr"],
+        ema_rate=params["ema_rate"],
+        log_interval=params["log_interval"],
+        save_interval=params["save_interval"],
+        resume_checkpoint=params["resume_checkpoint"],
+        use_fp16=params["use_fp16"],
+        fp16_scale_growth=params["fp16_scale_growth"],
         schedule_sampler=schedule_sampler,
-        weight_decay=args.weight_decay,
-        lr_anneal_steps=args.lr_anneal_steps,
-        max_train_steps=args.max_train_steps
+        weight_decay=params["weight_decay"],
+        lr_anneal_steps=params["lr_anneal_steps"],
+        max_train_steps=params["max_train_steps"]
     ).run_loop()
 
 
-def create_argparser():
+def get_default_params():
     defaults = dict(
         data_dir="",
         schedule_sampler="uniform",
@@ -79,9 +85,9 @@ def create_argparser():
         max_train_steps=100
     )
     defaults.update(model_and_diffusion_defaults())
-    parser = argparse.ArgumentParser()
-    add_dict_to_argparser(parser, defaults)
-    return parser
+    # parser = argparse.ArgumentParser()
+    # add_dict_to_argparser(parser, defaults)
+    return defaults
 
 
 if __name__ == "__main__":
