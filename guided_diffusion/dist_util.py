8c5c2bc883e7dd317ec6a733291390c15a0aa079
ge32woz <fcelik@sabanciuniv.edu>
2022-07-28 19:58:01 +0200

    added CUDA_VISIBLE_DEVICES suppression

--- guided_diffusion/dist_util.py
+++ guided_diffusion/dist_util.py
@@ -25,7 +25,11 @@ def setup_dist():
     """
     if dist.is_initialized():
         return
-    os.environ["CUDA_VISIBLE_DEVICES"] = f"{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}"
+
+    if os.environ.get("CUDA_VISIBLE_DEVICES", None) is None:
+        os.environ[
+            "CUDA_VISIBLE_DEVICES"
+        ] = f"{MPI.COMM_WORLD.Get_rank() % GPUS_PER_NODE}"
 
     comm = MPI.COMM_WORLD
     backend = "gloo" if not th.cuda.is_available() else "nccl"
